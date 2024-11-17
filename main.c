f3489d973f7fdaca4c4caf81ad78021b2b4231b7
Suiciniv01 <viniciusfranca567@gmail.com>
2024-11-17 12:54:07 -0300

    Atividade "Vetor Dinâmico" realizada. Aluno: Marcos Vinicius Costa de França.

--- main.c
+++ main.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    int n, i;
+    scanf("%d", &n);
+
+    if (n == 0){
+
+        printf("[vazio]");
+        return 0;
+
+    }
+
+    int *v = (int *) malloc(n * sizeof(int));
+
+    for (i = 0; i < n; i++){
+
+        scanf("%d", &v[i]);
+
+    }
+
+    printf("[");
+    for (i = 0; i < n; i++){
+        
+        printf("%d", v[i]);
+
+        if (i < n-1){
+
+            printf(",");
+
+        }
+        
+    }
+    printf("]");
+
+    free(v);
 
     return 0;
 }
